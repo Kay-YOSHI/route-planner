8297b2662106d2076f0d5ecb47f004f3f9e31234
Kay-YOSHI <ksk0110@gmail.com>
2024-03-12 22:38:41 +0900

    巡回セールスマン問題を解くスクリプト実装

--- route_planner.py
+++ route_planner.py
@@ -1,2 +1,123 @@
+"""Simple Travelling Salesperson Problem (TSP) between cities."""
+
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
 
 
+# データ生成
+def create_data_model():
+    """Stores the data for the problem."""
+    data = {}
+    data["distance_matrix"] = [
+        [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
+        [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
+        [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
+        [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
+        [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
+        [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
+        [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
+        [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
+        [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
+        [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
+        [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
+        [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
+        [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
+    ]  # 距離は整数
+    data["num_vehicles"] = 1
+    data["depot"] = 0  # ルートの始点と終点
+    return data
+
+
+# solution printer
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print(f"Objective: {solution.ObjectiveValue()} miles")
+    index = routing.Start(0)
+    plan_output = "Route for vehicle 0:\n"
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += f" {manager.IndexToNode(index)} ->"
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += f" {manager.IndexToNode(index)}\n"
+    print(plan_output)
+    plan_output += f"Route distance: {route_distance}miles\n"
+
+
+# ルートをリストに保存
+def get_routes(solution, routing, manager):
+    """Get vehicle routes from a solution and store them in an array."""
+    # Get vehicle routes and store them in a two dimensional array whose
+    # i,j entry is the jth location visited by vehicle i along its route.
+    routes = []
+    for route_nbr in range(routing.vehicles()):
+        index = routing.Start(route_nbr)
+        route = [manager.IndexToNode(index)]
+        while not routing.IsEnd(index):
+            index = solution.Value(routing.NextVar(index))
+            route.append(manager.IndexToNode(index))
+        routes.append(route)
+    return routes
+
+
+def main():
+    # データ生成
+    data = create_data_model()
+
+    # Index Managerを作成
+    # nodeとindexの紐づけを管理してくれるもの
+    # node: 我々が認識している地点番号
+    # index: ソルバー内部で処理される際に用いられるもの
+    manager = pywrapcp.RoutingIndexManager(
+        len(data["distance_matrix"]),  # 地点数
+        data["num_vehicles"],  # 車両数
+        data["depot"],  # 始点/終点のindex
+    )
+
+    # Routing Modelを作成
+    routing = pywrapcp.RoutingModel(manager)
+
+    # 距離コールバック作成
+    # from_nodeからto_nodeへの移動コストをdistance_matrixから見つけるコールバック
+    # コールバック関数：ある関数を呼び出すときに引数に指定する別の関数
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data["distance_matrix"][from_node][to_node]
+
+    # 距離コールバックをソルバーに登録
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Arc Costの設定
+    # 2地点間の移動コスト（ここでは距離そのもの）
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # いろいろ設定を追加できるっぽい
+
+    # 解法の設定
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+
+    # first_solution_strategy（初期解の求め方）
+    # PATH_CHEAPEST_ARC: 今いる地点から最短距離の地点にルートをつないでいくイメージ
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+    )
+
+    # 最適化実行
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # 解が見つかれば表示
+    if solution:
+        print_solution(manager, routing, solution)
+    else:
+        print("NO SOLUTION FOUND")
+
+    # 巡回する順序に並んだ地点のリスト
+    optimal_routes = get_routes(solution, routing, manager)[0]
+
+
+if __name__ == "__main__":
+    main()
