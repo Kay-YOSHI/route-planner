92ad0e5002af0580760bcfe0ab0f2bea4ff3046c
Kay-YOSHI <ksk0110@gmail.com>
2024-03-15 22:46:39 +0900

    最適経路をmapに描画

--- route_planner.py
+++ route_planner.py
@@ -4,10 +4,13 @@ import pandas as pd
 import numpy as np
 import sklearn.preprocessing as sp
 from sklearn.preprocessing import MinMaxScaler
+import time
 
 import folium  # Leaflet.js を使用した地理空間データの可視化
 from folium import plugins  # folium ライブラリのための追加機能やプラグインを提供
 
+import osmnx as ox  # OpenStreetMap のデータを取得、構築、分析し、ネットワーク分析を行う
+import networkx as nx  # 複雑なネットワークの作成、操作、研究の用途
 from ortools.constraint_solver import (
     pywrapcp,
 )  # OR-Tools で、制約プログラミングをサポートする Python ラッパー
@@ -41,23 +44,8 @@ def create_data_model():
     return data
 '''
 
-"""
-folium でマップを作成する。
-:パラメータ
-    param dtf: pandas
-    param (y,x): str - (緯度，経度) を要素とする列
-    param starting_point: list - (緯度，経度) を指定
-    param tiles: str - "cartodbpositron", "OpenStreetMap", "Stamen Terrain", "Stamen Toner".
-    param popup: str - クリックされたときにポップアップするテキストを指定する列
-    :param size: str - サイズを変数で指定する列，None の場合は size=5 となる
-    :param color: str - 変数 color を指定する列，None の場合はデフォルトの色になる
-    :param lst_colors: list - 色の列が None でない場合に利用される、複数の色のリスト
-    param marker: str - 変数 marker を指定する列，最大 7 個のユニークな値を取る
-:戻り値
-    表示するマップオブジェクト
-"""
 
-
+# map作成
 def plot_map(
     dtf,
     y,
@@ -72,6 +60,21 @@ def plot_map(
     lst_colors=None,
     marker=None,
 ):
+    """
+    folium でマップを作成する。
+    :パラメータ
+        param dtf: pandas
+        param (y,x): str - (緯度，経度) を要素とする列
+        param starting_point: list - (緯度，経度) を指定
+        param tiles: str - "cartodbpositron", "OpenStreetMap", "Stamen Terrain", "Stamen Toner".
+        param popup: str - クリックされたときにポップアップするテキストを指定する列
+        :param size: str - サイズを変数で指定する列，None の場合は size=5 となる
+        :param color: str - 変数 color を指定する列，None の場合はデフォルトの色になる
+        :param lst_colors: list - 色の列が None でない場合に利用される、複数の色のリスト
+        param marker: str - 変数 marker を指定する列，最大 7 個のユニークな値を取る
+    :戻り値
+        表示するマップオブジェクト
+    """
 
     data = dtf.copy()
 
@@ -141,8 +144,6 @@ def plot_map(
             ).add_to(map_),
             axis=1,
         )
-
-    print(data)
 
     ## tiles
     # タイルセットを追加
@@ -232,35 +233,18 @@ def plot_map(
     return map_
 
 
-# ルート（リスト）と総走行距離を取得
-def get_route_and_distance(solution, routing, manager):
-    # ドライバー#0 の開始インデックスを取得
-    index = routing.Start(0)
-    # ルート（リスト），総走行距離
-    route_idx, route_distance = [], 0
-    # ルートの各ノードを走査
-    while not routing.IsEnd(index):
-        # 現在のインデックスに対応するノードをリストに追加
-        route_idx.append(manager.IndexToNode(index))
-        previous_index = index
-        # 次のインデックスを取得
-        index = solution.Value(routing.NextVar(index))
-        # 距離を取得し，ルートの総距離に加算
-        route_distance += routing.GetArcCostForVehicle(
-            from_index=previous_index, to_index=index, vehicle=0
-        )
-    return route_idx, route_distance
-
-
 def main():
+
+    # 時間計測開始
+    start_time = time.perf_counter()
+
     # データ生成
-    # TODO: 地点名入力⇒緯度経度取得⇒リストに格納
+    # TODO: (streamlit)地点名入力⇒緯度経度取得⇒リストに格納
     # data = create_data_model()
     dtf_list = [
         [0, "自宅", 35.853237566583864, 139.52565241326212],
-        [1, "大宮けんぽグラウンド", 35.89021146717185, 139.570908903868],
-        [2, "ららぽーと富士見", 35.85995667233151, 139.5483715641925],
-        [3, "イオンタウンふじみ野", 35.88316939483739, 139.52213157793798],
+        [1, "ららぽーと富士見", 35.85995667233151, 139.5483715641925],
+        [2, "イオンタウンふじみ野", 35.88316939483739, 139.52213157793798],
     ]
     cols = ["id", "Name", "y", "x"]  # y: 緯度, x: 経度
     dtf = pd.DataFrame(data=dtf_list, columns=cols)
@@ -268,11 +252,13 @@ def main():
     # 基点となる場所の設定
     i = 0  # id=0の地点を基点とする
     dtf["base"] = dtf["id"].apply(lambda x: 1 if x == i else 0)
-    start = dtf[dtf["base"] == 1][["y", "x"]].values
-    print(f"start = {start}")
-    print(dtf.head())
+    start = dtf[dtf["base"] == 1][["y", "x"]].values[0]
+    # end = start  # 終点＝始点とする
+    # print(f"From: {start} --> To: {end}")
+    # print(dtf.head())
 
     # 地図上に地点を表示
+    """
     map_ = plot_map(
         dtf,
         y="y",
@@ -285,62 +271,190 @@ def main():
         lst_colors=["blue", "red"],
     )
     map_.save("map.html")  # map.htmlをブラウザで開けば地図が見られる
+    """
 
-    '''
-    # Index Managerを作成
-    # nodeとindexの紐づけを管理してくれるもの
-    # node: 我々が認識している地点番号
-    # index: ソルバー内部で処理される際に用いられるもの
+    # ネットワークグラフの作成
+    # - 'start'の位置から半径10000メートル以内の道路ネットワークを取得
+    # - network_type = "drive" or "bike" or "walk"
+    G = ox.graph_from_point(start, dist=10000, network_type="drive")
+
+    # ネットワークグラフの各エッジ（道路）に情報を追加
+    G = ox.add_edge_speeds(G)  # 速度
+    G = ox.add_edge_travel_times(G)  # 旅行時間
+
+    # ネットワークグラフのプロット
+    #   - 背景色: 黒
+    #   - ノードのサイズ: 5
+    #   - ノード色: 白
+    #   - 図のサイズ: 幅16インチ、高さ8インチ
+    # fig, ax = ox.plot_graph(
+    #    G, bgcolor="black", node_size=5, node_color="white", figsize=(16, 8)
+    # )
+
+    # 道路ネットワークGから各地点に対応する（最も近い）ノードを取得
+    dtf["node"] = dtf[["y", "x"]].apply(
+        lambda x: ox.distance.nearest_nodes(G, x[1], x[0]), axis=1
+    )
+    # print(dtf.head())
+
+    # 重複するノードを削除し、最初に出現するもののみを保持
+    # dtf = dtf.drop_duplicates("node", keep='first')
+
+    # 距離計算関数
+    def f(a, b):
+        """
+        ノードaからbの最短経路の長さを計算
+        """
+        try:
+            d = nx.shortest_path_length(
+                G, source=a, target=b, method="dijkstra", weight="length"
+            )
+        except:
+            d = np.nan
+        return d
+
+    # 距離行列を作成
+    distance_matrix = np.asarray(
+        [[f(a, b) for b in dtf["node"].tolist()] for a in dtf["node"].tolist()]
+    )
+
+    # 距離行列を Pandas データフレームに変換
+    distance_matrix = pd.DataFrame(
+        distance_matrix, columns=dtf["Name"].values, index=dtf["Name"].values
+    )
+    # print(distance_matrix.head())
+
+    # 始点ノード，総巡回地点，ドライバー数
+    start_node = dtf[dtf["id"] == i]["node"][0]  # 始点ノード
+    lst_nodes = dtf["node"].tolist()  # ノードのリスト
+    drivers = 1  # ドライバー数
+    # print(
+    #    f"始点ノード: {start_node}, 総巡回地点: {len(lst_nodes)-1}, ドライバー数: {drivers}"
+    # )
+
+    # Index Manager（nodeとindexの紐づけを管理してくれるもの）を作成
+    #  - node: 我々が認識している地点番号
+    #  - index: ソルバー内部で処理される際に用いられるもの
     manager = pywrapcp.RoutingIndexManager(
-        len(data["distance_matrix"]),  # 地点数
-        data["num_vehicles"],  # 車両数
-        data["depot"],  # 始点/終点のindex
+        len(lst_nodes),  # 地点数
+        drivers,  # ドライバー数
+        lst_nodes.index(start_node),  # 始点/終点のindex
     )
 
     # Routing Modelを作成
-    routing = pywrapcp.RoutingModel(manager)
+    model = pywrapcp.RoutingModel(manager)
 
     # 距離コールバック（距離取得関数，距離コスト評価関数）作成
     # from_nodeからto_nodeへの移動（距離）コストをdistance_matrixから見つけるコールバック
     # コールバック関数：ある関数を呼び出すときに引数に指定する別の関数
-    def distance_callback(from_index, to_index):
+    def get_distance(from_index, to_index):
         """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data["distance_matrix"][from_node][to_node]
+        return distance_matrix.iloc[from_index, to_index]
 
     # 距離コールバックをRouting Modelに登録
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    distance = model.RegisterTransitCallback(get_distance)
 
     # すべての車両に距離コスト評価関数を設定
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    model.SetArcCostEvaluatorOfAllVehicles(distance)
 
     # MEMO: いろいろ設定（制約条件）を追加できるっぽい
 
     # デフォルトのルーティング検索パラメータを取得
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    parameters = pywrapcp.DefaultRoutingSearchParameters()
 
     # 初期解を生成する戦略（first_solution_strategy）を設定
     # PATH_CHEAPEST_ARC:
     # ・最もコストの低いアーク（経路のセグメント）を選択する戦略
     # ・今いる地点から最短距離の地点にルートをつないでいくイメージ
-    search_parameters.first_solution_strategy = (
+    parameters.first_solution_strategy = (
         routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
     )
 
     # 最適化実行
-    solution = routing.SolveWithParameters(search_parameters)
+    solution = model.SolveWithParameters(parameters)
+
+    # ルート（リスト）と総走行距離を取得
+    def get_route_and_distance(solution, model, manager):
+        # ドライバー#0 の開始インデックスを取得
+        index = model.Start(0)
+        # ルート（リスト），総走行距離
+        route_idx, route_distance = [], 0
+        # ルートの各ノードを走査
+        while not model.IsEnd(index):
+            # 現在のインデックスに対応するノードをリストに追加
+            route_idx.append(manager.IndexToNode(index))
+            previous_index = index
+            # 次のインデックスを取得
+            index = solution.Value(model.NextVar(index))
+
+            # 距離を更新
+            try:
+                # distance_matrixから距離を取得し，ルートの総距離に加算
+                route_distance += get_distance(previous_index, index)
+            except:
+                # エラーを吐いた場合は距離を取得
+                route_distance += model.GetArcCostForVehicle(
+                    from_index=previous_index, to_index=index, vehicle=0
+                )
+        route_idx.append(manager.IndexToNode(index))
+        return route_idx, route_distance
 
     # 最適ルートと総走行距離を取得
-    optimal_route, optimal_distance = get_route_and_distance(solution, routing, manager)
-    print(f"Route: {optimal_route}")
-    print(f"Total Distance: {optimal_distance} miles")
+    optimal_route, optimal_distance = get_route_and_distance(solution, model, manager)
 
-    # TODO:
     # optimal_routes（インデックスのリスト）をノードのリストに変換
-    # 任意の2ノード間の最短経路を算出し，地図上にルートを描画
-    '''
+    lst_route = [lst_nodes[i] for i in optimal_route]
+
+    # ノード間の経路を取得する関数
+    def get_path_between_nodes(lst_route):
+        lst_paths = []
+        for i in range(len(lst_route)):
+            try:
+                # 現在のノードと次のノードを取得
+                a, b = lst_route[i], lst_route[i + 1]
+            except:
+                # 次のノードがない場合はループを終了
+                break
+            try:
+                # グラフ G 上でノード a からノード b までの最短経路を計算
+                # ダイクストラ法を使用し、重みは 'travel_time'（移動時間）で計算
+                path = nx.shortest_path(
+                    G, source=a, target=b, method="dijkstra", weight="travel_time"
+                )
+                # 経路が存在する場合（長さが 1 より大きい場合）、リストに追加
+                if len(path) > 1:
+                    lst_paths.append(path)
+            except:
+                # 経路が見つからない場合は次のループへ
+                continue
+        return lst_paths
+
+    # ルート内の各ノード間の経路を取得
+    lst_paths = get_path_between_nodes(lst_route)
+
+    # 地図上にルートを描画
+    map_ = plot_map(
+        dtf,
+        y="y",
+        x="x",
+        start=start,
+        zoom=12,
+        tiles="openstreetmap",
+        popup="Name",
+        color="base",
+        lst_colors=["blue", "red"],
+    )
+    for path in lst_paths:
+        ox.plot_route_folium(G, route=path, route_map=map_, color="red", weight=2)
+    map_.save("optimal_route.html")  # map.htmlをブラウザで開けば地図が見られる
+
+    # 時間計測終了
+    end_time = time.perf_counter()
+
+    # Results
+    print(f"Route: {optimal_route}")
+    print(f"Total Distance: {round(optimal_distance/1000, 2)} km")
+    print(f"Computation time: {'{:.2f}'.format((end_time-start_time)/60)} min.")
 
 
 if __name__ == "__main__":
